a315cb253eb8b0b447e1b38b7e58c7f8956ba7a5
Alexandre-Marques-apps <alexandre300694@gmail.com>
2026-03-18 11:01:33 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+jsexport const metadata = {
+  title: 'Salão de Beleza',
+  description: 'Agendamento online',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
